c82abe46ab66ea65810ad2d2349dc1c8ca4aa5c6
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-13 11:12:11 -0500

    deleted unneccesary files

--- db/migrate/20130922182216_change_challenges_order_column.rb
+++
@@ -1,9 +0,0 @@
-class ChangeChallengesOrderColumn < ActiveRecord::Migration
-  def up
-    add_column :challenges, :challenge_order, :integer
-  end
-
-  def down
-    remove_column :challenges, :order, :integer
-  end
-end
